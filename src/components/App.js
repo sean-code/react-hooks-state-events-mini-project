30beb04d99d4b8b75b95267515eefc38187aea0d
sean-code <john.nganga@student.moringaschool.com>
2022-09-26 14:04:08 +0300

    Coded along

--- src/components/App.js
+++ src/components/App.js
@@ -8,14 +8,20 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
-  return (
-    <div className="App">
-      <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
-    </div>
-  );
+	const [tasks, setTasks] = React.useState(TASKS);
+	const handleDeleteTasks = (task) => {
+		console.log(task);
+		let newTasks = tasks.filter((t) => t.text !== task);
+		setTasks(newTasks);
+	};
+	return (
+		<div className="App">
+			<h2>My tasks</h2>
+			<CategoryFilter />
+			<NewTaskForm />
+			<TaskList tasks={tasks} handleDeleteTasks={handleDeleteTasks} />
+		</div>
+	);
 }
 
 export default App;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,20 @@
 import React from "react";
-
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
+import Task from "./Task";
+function TaskList({ tasks, handleDeleteTasks }) {
+	console.log("My tasks Element", tasks);
+	return (
+		<div className="tasks">
+			{/* display a list of tasks using Task component */}
+			{tasks.map((task) => (
+				<Task
+					key={task.text}
+					text={task.text}
+					handleDeleteTasks={handleDeleteTasks}
+					category={task.category}
+				/>
+			))}
+		</div>
+	);
 }
 
 export default TaskList;
